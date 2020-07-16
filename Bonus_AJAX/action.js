d8128f4b6def6400f3e7470cb47a3ffc5ef270a8
Thien Pham <phamanhthien1907@gmail.com>
2020-07-16 19:47:49 +0700

    Update async await

--- Bonus_AJAX/action.js
+++ Bonus_AJAX/action.js
@@ -3,39 +3,43 @@ $(() => {
     $('.fetching-table').hide();
     $('#emptyTableBtn').click(() => {
         $('.fetching-table').toggle('slow');
-    })
+    });
 
     //table has data
     $('#fetchingAndShowTableBtn').click(() => {
         $('.fetching-table').hide('slow', () => {
-            fetchingData(show);
+            fetchingData().then(show);
         });
-    })
+    });
 
-    function fetchingData(callback) {
+    async function fetchingData() {
+        console.log('No data !');
         $.ajax({
             type: 'GET',
             url: 'https://spring-student-api.herokuapp.com/info?apiKey=eaf118ab-babf-43ed-a788-b66d2f0ca00c',
             dataType: 'json',
             data: [],
-            success: function fetching(data) {
-                let result = {
-                    listStudent: data
-                };
+            success:
+                    function fetching(data) {
+                        let result = {
+                            listStudent: data
+                        };
 
-                let loopStudentTemplate = $('#loop-student-template').html();
-                let loopStudentTemplateCompile = Handlebars.compile(loopStudentTemplate)
-                let loopStudentTemplateWithData = loopStudentTemplateCompile(result);
-               $('#list-student').html(loopStudentTemplateWithData);
-            },
+                        let loopStudentTemplate = $('#loop-student-template').html();
+                        let loopStudentTemplateCompile = Handlebars.compile(loopStudentTemplate);
+                        let loopStudentTemplateWithData = loopStudentTemplateCompile(result);
+
+                        $('#list-student').html(loopStudentTemplateWithData);
+                    },
+
             error: function (data) {
-                console.log(error)
+                console.log('error');
             }
         });
-        callback();
     }
 
     function show() {
-        $('.fetching-table').toggle('3000');
+        $('.fetching-table').slideToggle('slow');
+        console.log('Loading data done !')
     }
 });
