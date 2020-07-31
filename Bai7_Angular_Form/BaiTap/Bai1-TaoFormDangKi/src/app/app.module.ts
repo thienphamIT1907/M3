65bf4722e3caf6214e668e5236987be412e7b262
Thien Pham <phamanhthien1907@gmail.com>
2020-07-31 09:13:09 +0700

    Hoan thanh bai tap: Tao Form dang ki

---
+++ Bai7_Angular_Form/BaiTap/Bai1-TaoFormDangKi/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
